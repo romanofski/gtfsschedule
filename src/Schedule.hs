942399cbd9d1710a03c422a2a877402259bc98da
Roman Joost <rjoost@redhat.com>
2015-11-02 14:00:16 +1000

    WIP parsing csv files like stop_times for schedule

--- src/Schedule.hs
+++ src/Schedule.hs
@@ -1,15 +1,90 @@
+{-# LANGUAGE DeriveGeneric #-}
 -- | the GTFS schedule
 module Schedule
-    ( lookupStopInfo
+    (getSchedule
+    , printSchedule
+    , isStation
+    , parseCSV
+    , filterRecords
+    , StopTime(..)
     ) where
 
-import Data.GTFS.Parse (parseFile)
+import Data.Csv (FromNamedRecord(..)
+                , FromField(..)
+                , runParser
+                , (.:))
+import Data.Csv.Streaming
+import Data.Vector (Vector)
+import Data.Time.LocalTime (TimeOfDay)
+import Data.Time.Format (parseTimeM, defaultTimeLocale)
+import Control.Applicative (empty)
+import GHC.Generics
+import qualified Data.Vector as V
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Foldable as F
 
--- | returns stop name from given stopid
--- TODO: currently hard coded to roma street platform 8 (600029)
+
+data StopTime = StopTime { trip_id :: !String
+                         , arrival_time :: !TimeOfDay
+                         , departure_time :: !TimeOfDay
+                         , stop_id :: !String
+                         , stop_sequence :: !Int
+                         , pickup_type :: !Int
+                         , drop_off_type :: !Int
+                         }
+              deriving (Eq, Generic, Show)
+
+instance FromNamedRecord StopTime
+
+instance FromField TimeOfDay where
+  parseField s = case parseTimeM True defaultTimeLocale "%T" (C.unpack s) of
+    Just t -> pure t
+    Nothing -> empty
+
+
+-- | parses CSV file and returns either a parser error or a list of stop times
 --
-lookupStopName ::
+parseCSV ::
+  B.ByteString
+  -> IO (Either String (Records StopTime))
+parseCSV contents =
+  case decodeByName contents of
+    Left errmsg -> return $ Left errmsg
+    Right (_, r) -> return $ Right r
+
+filterRecords ::
+  Records StopTime
+  -> [StopTime]
+filterRecords = F.foldr filterStation []
+
+-- | TODO: use isStation
+filterStation ::
+  StopTime
+  -> [StopTime]
+  -> [StopTime]
+filterStation x@(StopTime {stop_id = sID}) a = if sID == "600029" then x : a else a
+
+-- | returns True if given station id is part of the stop time record
+--
+isStation ::
   String
-  -> String
-lookupStopName stopId = filter
-  where stopTimes = parseFile "stop_times.txt"
+  -> StopTime
+  -> Bool
+isStation x StopTime { stop_id = sID } = sID == x
+
+-- | returns all available stop information
+--
+getSchedule ::
+  String
+  -> Vector StopTime
+  -> Vector StopTime
+getSchedule stopID = V.filter p
+  where p StopTime { stop_id = sID } = sID == stopID
+
+-- | prints list of StopTimes as schedule
+--
+printSchedule ::
+  Vector StopTime
+  -> IO (Vector ())
+printSchedule = V.mapM print
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,11 @@
 module Main where
 
 import Test.Tasty (defaultMain, TestTree, testGroup)
-import Test.Tasty.HUnit (assertBool, testCase)
-import qualified Data.ByteString.Lazy as Lazy
-import Com.Google.Transit.Realtime.TripUpdate.StopTimeUpdate
+import Test.Tasty.HUnit (assertEqual, testCase)
+import qualified Data.ByteString.Lazy as B
+import Data.Time.LocalTime (TimeOfDay(..))
 
-import Lib (getSchedule)
+import Schedule (parseCSV, filterRecords, StopTime(..))
 
 tests ::
   TestTree
@@ -14,20 +14,61 @@ tests = testGroup "unit tests" [unitTests]
 unitTests ::
   TestTree
 unitTests = testGroup "schedule tests"
-            [ testgetSchedule ]
+            [ testParsesCSVWithInvalidData
+            , testParsesCSV
+            ]
 
-testHasDeparture ::
+testParsesCSVWithInvalidData ::
   TestTree
-testHasDeparture =
-  testCase "correctly shows departure" $ assertBool "expected stopid not empty" departure
-  where departure = case getSchedule getGTFSFeed of
-          Nothing -> Nothing
-          Just msg -> getDeparture msg
+testParsesCSVWithInvalidData =
+  testCase "correctly ignores invalid csv data" $ do
+    parsed <- getFilteredStopTimes invalidCSVStopTimes
+    assertEqual "expecting one record" expected parsed
+    where expected = [ StopTime { trip_id = "3-QR2015"
+                                 , arrival_time = TimeOfDay 8 03 00
+                                 , departure_time = TimeOfDay 8 03 00
+                                 , stop_id = "600029"
+                                 , stop_sequence = 20
+                                 , pickup_type = 0
+                                 , drop_off_type = 0}
+                     ]
+
+testParsesCSV ::
+  TestTree
+testParsesCSV =
+  testCase "parses csv data successfully" $ do
+    parsed <- getFilteredStopTimes validCSVStopTimes
+    assertEqual "expecting two records" expected parsed
+     where expected = [ StopTime { trip_id = "3-QR2015"
+                                 , arrival_time = TimeOfDay 8 03 00
+                                 , departure_time = TimeOfDay 8 03 00
+                                 , stop_id = "600029"
+                                 , stop_sequence = 20
+                                 , pickup_type = 0
+                                 , drop_off_type = 0}
+                      , StopTime { trip_id = "4-QR2015"
+                                 , arrival_time = TimeOfDay 8 04 00
+                                 , departure_time = TimeOfDay 8 04 00
+                                 , stop_id = "600029"
+                                 , stop_sequence = 20
+                                 , pickup_type = 0
+                                 , drop_off_type = 0}
+                      ]
 
 -- helpers
 --
-getGTFSFeed :: IO Lazy.Bytestring
-getGTFSFeed = readFile "data/SEQ"
+invalidCSVStopTimes :: IO B.ByteString
+invalidCSVStopTimes = B.readFile "test/data/invalid_stop_times.txt"
+
+validCSVStopTimes :: IO B.ByteString
+validCSVStopTimes = B.readFile "test/data/valid_stop_times.txt"
+
+getFilteredStopTimes f = do
+  c <- f
+  parsed <- parseCSV c
+  case parsed of
+    Left _ -> return []
+    Right r -> return $ filterRecords r
 
 main ::
   IO ()
