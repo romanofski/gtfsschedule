fcf8b3496f998613ad60267d9b15e66d810c928c
Roman Joost <rjoost@redhat.com>
2015-12-08 09:00:07 +1000

    add option parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,54 @@
 module Main where
 
-import System.Environment (getArgs)
-import Schedule (printSchedule)
+import Options.Applicative.Builder ( switch
+                                   , long
+                                   , help
+                                   , (<>)
+                                   , metavar
+                                   , strOption
+                                   , fullDesc
+                                   , progDesc
+                                   , header
+                                   , info)
+import Options.Applicative.Types (Parser)
+import Options.Applicative.Extra ( execParser
+                                 , helper)
+import Schedule ( printSchedule
+                , filterSchedule)
 import qualified Data.ByteString.Lazy as B
 
-main :: IO ()
-main = do
-  (fp:_) <- getArgs
+
+data Options = Options { filterStations :: Bool
+                       , stationID :: String
+                       , stationFilePath :: FilePath
+                       }
+
+optionParser ::
+  Parser Options
+optionParser = Options
+               <$> switch
+               ( long "filterStation"
+                 <> help "Wether filter on station id and write them as CSV encoded to STDOUT")
+               <*> strOption
+               ( long "stationID"
+                 <> metavar "sID"
+                 <> help "Station ID to show the schedule for")
+               <*> strOption
+               ( long "stopTimesTxt"
+                 <> metavar "stopTimes"
+                 <> help "filepath to stop_times.txt csv data to read from")
+
+runSchedule :: Options -> IO ()
+runSchedule (Options False sID fp) = do
   contents <- B.readFile fp
-  printSchedule "600029" contents
+  printSchedule sID contents
+runSchedule (Options True sID fp) = do
+  contents <- B.readFile fp
+  filterSchedule sID contents
+
+main :: IO ()
+main = execParser opts >>= runSchedule
+  where opts = info (helper <*> optionParser)
+               ( fullDesc
+                 <> progDesc "shows schedule times GTFS style"
+                 <> header "gtfs - a gtfs enabled brisbane transport schedule")
