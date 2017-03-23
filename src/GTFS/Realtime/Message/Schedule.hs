ef481a0cdde282bb2d74b8eaed6d65e54ac9d64a
Roman Joost <roman@bromeco.de>
2017-03-23 19:09:24 +1000

    Add license header to every file

--- src/GTFS/Realtime/Message/Schedule.hs
+++ src/GTFS/Realtime/Message/Schedule.hs
@@ -1,5 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternGuards     #-}
+{-
+Copyright (C) - 2017 Róman Joost <roman@bromeco.de>
+
+This file is part of gtfsschedule.
+
+gtfsschedule is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gtfsschedule is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gtfsschedule.  If not, see <http://www.gnu.org/licenses/>.
+-}
 {- | Module to parse the real time feed for real time updates
 
 This module uses protocol buffers to parse the feedmessage in order to update
