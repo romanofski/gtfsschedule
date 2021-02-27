1add6b27792efabc4dcdfe17396b05aeca0874cf
Róman Joost <roman@bromeco.de>
2021-02-27 14:45:10 +1000

    Deprecate stack and document nix

---
+++ default.nix
@@ -0,0 +1,64 @@
+# Building gtfsschedule
+#
+# You should be able to simply invoke:
+#
+# $ nix-build
+#
+# or, to be explicit:
+#
+# $ nix-build default.nix
+#
+# in gtfsschedule's root directory in order to build gtfsschedule. You'll find the binary under:
+#
+# $ ls result/bin/gtfsschedule
+#
+# if the build was successful.
+#
+#
+# Choosing a different compiler than the default
+#
+# In order to choose a different compiler, invoke nix build like so (escaping
+# the quotes is needed, since we're passing a string literal):
+#
+# $ nix-build --arg compiler \"ghc442\"
+#
+# Use as a development environment
+#
+# $ nix-shell default.nix
+#
+{ compiler ? null, nixpkgs ? null, with-icu ? false }:
+
+with (import .nix/nixpkgs.nix { inherit compiler nixpkgs; });
+
+let
+  envPackages = self: [ self.gtfsschedule ];
+  env = haskellPackages.ghcWithPackages envPackages;
+  nativeBuildTools = with pkgs.haskellPackages; [
+    cabal-install
+    cabal2nix
+    ghcid
+    hlint
+    pkgs.gnumake
+    pkgs.asciidoctor
+    pkgs.python3Packages.pygments
+  ];
+in
+if pkgs.lib.inNixShell
+then haskellPackages.shellFor {
+  withHoogle = true;
+  packages = haskellPackages: [ haskellPackages.gtfsschedule ];
+  nativeBuildInputs = haskellPackages.gtfsschedule.env.nativeBuildInputs ++ nativeBuildTools;
+}
+else {
+  gtfsschedule = pkgs.stdenv.mkDerivation {
+    name = "gtfsschedule-with-packages-${env.version}";
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    buildCommand = ''
+      mkdir -p $out/bin
+      makeWrapper ${env}/bin/gtfsschedule $out/bin/gtfsschedule \
+      --set NIX_GHC "${env}/bin/ghc"
+    '';
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+  };
+}
