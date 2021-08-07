bfdf0acc4ae5b2202090db33cae77437bd427852
Róman Joost <roman@bromeco.de>
2021-08-07 18:22:07 +1000

    Update nixpkgs and fix up hprotoc
    Well it's not a fix, but rather a workaround, but it'll get me there.

--- .nix/hprotoc.nix
+++ .nix/hprotoc.nix
@@ -1,11 +1,20 @@
 { mkDerivation, alex, array, base, binary, bytestring, containers
 , directory, filepath, haskell-src-exts, lib, mtl, parsec
-, protocol-buffers, protocol-buffers-descriptor, utf8-string
-}:
+, protocol-buffers, protocol-buffers-descriptor, utf8-string, fetchgit }:
 mkDerivation {
   pname = "hprotoc";
   version = "2.4.17";
-  src = ../../protocol-buffers/hprotoc;
+  src = fetchgit {
+    url = "https://github.com/k-bx/protocol-buffers.git";
+    sha256 = "0hph7l78iwkz5bs6z3sdjgcgkczmwr1vydphdbkb8i3r281sq8x2";
+    rev = "9b109f2fefa75f45911edc967db145505bfc7a1b";
+    fetchSubmodules = true;
+  };
+  # This is yikes!!! Couldn't find a better way to build the subdirectory.
+  preConfigure = ''
+    rm -rf Text protocol-buffers.cabal
+    mv hprotoc/* .
+  '';
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
