affaaf6cc97e2c38fdaa1ecfa3a1aa580547068d
Róman Joost <roman@bromeco.de>
2021-08-07 15:43:02 +1000

    Add forgotten hprotoc nix definition

---
+++ .nix/hprotoc.nix
@@ -0,0 +1,26 @@
+{ mkDerivation, alex, array, base, binary, bytestring, containers
+, directory, filepath, haskell-src-exts, lib, mtl, parsec
+, protocol-buffers, protocol-buffers-descriptor, utf8-string
+}:
+mkDerivation {
+  pname = "hprotoc";
+  version = "2.4.17";
+  src = ../../protocol-buffers/hprotoc;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    array base binary bytestring containers directory filepath
+    haskell-src-exts mtl parsec protocol-buffers
+    protocol-buffers-descriptor utf8-string
+  ];
+  libraryToolDepends = [ alex ];
+  executableHaskellDepends = [
+    array base binary bytestring containers directory filepath
+    haskell-src-exts mtl parsec protocol-buffers
+    protocol-buffers-descriptor utf8-string
+  ];
+  executableToolDepends = [ alex ];
+  homepage = "https://github.com/k-bx/protocol-buffers";
+  description = "Parse Google Protocol Buffer specifications";
+  license = lib.licenses.bsd3;
+}
